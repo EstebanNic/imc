317f00003cf30594dad05a54cee67d279f9380b8
Esteban S. <estebansambachi@gmail.com>
2024-12-10 09:29:46 -0500

    Agregar archivos de proyecto.

---
+++ imc/Controllers/imcController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using imc.Models;
+
+namespace imc.Controllers
+{
+    public class imcController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult CalcularIMC(imcModels models){
+            if (ModelState.IsValid)
+            {
+                return View("Resultado",models);
+            }
+            return View("Index");
+
+
+        
+        }
+    }
+}
---
+++ imc/Models/imcModels.cs
@@ -0,0 +1,48 @@
+﻿namespace imc.Models
+{
+    public class imcModels
+    {
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+
+        public double IMC
+        {
+            get
+            {
+
+                return Peso/(Altura*Altura);
+               
+            }
+
+
+        }
+
+       public string CategoriaIMC
+        {
+            get
+            {
+                double imc = IMC;
+                if (imc > 18.5)
+                {
+                    return "Bajo de peso";
+                }
+                else if (imc >= 18.5 && imc < 24.9)
+                {
+                    return "Peso normal";
+                }
+                else if (imc >= 25 && imc < 29.9)
+                {
+                    return "Peso alto";
+                }
+                else
+                {
+                    return "Bajo de peso";
+                }
+            }
+
+        }
+}
+}
